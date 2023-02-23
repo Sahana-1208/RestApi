59df90b3b7ccdef65c8bf965febca73864dd04a0
Sahana S <125949968+Sahana-1208@users.noreply.github.com>
2023-02-23 15:10:56 +0530

    day 1&2

---
+++ day1,2/src/main/java/com/example/demo/color.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class color {
+	
+	@GetMapping("/color")
+	public String getMyFav(){
+		String yourFavColor="Green";
+		return "My favorite color is "+yourFavColor;
+	}
+
+}
